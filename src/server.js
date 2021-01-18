842651a98eb350d842c5557edf26f694abf9e6f2
Lip Cowan <lipcowan@lipco.dev>
2021-01-18 16:38:13 -0700

    working on heroku deployment

--- src/server.js
+++ src/server.js
@@ -1,6 +1,22 @@
+const knex = require('knex')
 const app = require('./app')
-const {PORT} = require('./config')
+const {PORT, DATABASE_URL} = require('./config')
 
+const db = knex({
+  client: 'pg',
+  connection: DATABASE_URL
+})
+
+// needed for heroku
+const { Pool } = require('pg')
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+})
+
+app.set('db', db)
 
 app.listen(PORT, () => {
   console.log(`Server listening at http://localhost:${PORT}`)

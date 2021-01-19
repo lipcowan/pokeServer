cfeceacd038b33dc048a6e685858439de2cc1970
Lip Cowan <lipcowan@lipco.dev>
2021-01-18 17:09:04 -0700

    updating

--- src/app.js
+++ src/app.js
@@ -1,11 +1,15 @@
 require('dotenv').config()
 const express = require('express')
+const errorHandler = require('./errorHandler')
 const morgan = require('morgan')
 const cors = require('cors')
 const helmet = require('helmet')
+// we need this for logging purposes
 const { NODE_ENV } = require('./config')
 
+// create app
 const app = express()
+
 
 const morganOption = (NODE_ENV === 'production')
   ? 'tiny'
@@ -15,21 +19,12 @@ app.use(morgan(morganOption))
 app.use(helmet())
 app.use(cors())
 
+// to test app is working 
 app.get('/', (req, res) => {
-  res.send('Hello, world!')
+  res.send('Welcome to the pokeserver!')
 })
 
 
- app.use(function errorHandler(error, req, res, next) {
-   let response
-   if (NODE_ENV === 'production') {
-     response = { error: { message: 'server error' } }
-   } else {
-     console.error(error)
-     response = { message: error.message, error }
-   }
-   res.status(500).json(response)
-})
-
+app.use(errorHandler)
 
 module.exports = app
